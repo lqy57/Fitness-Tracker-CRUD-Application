afa1cbc701d0d611f3497fb093869e19da6fd431
rominadouk <douk.romina@gmail.com>
2023-02-27 10:20:04 -0800

    updated img data with icons

--- models/fitnessData.js
+++ models/fitnessData.js
@@ -1,35 +1,35 @@
 const fitness = [
     {
         title: 'Workout1',
-        img: String,
+        img: "https://i.imgur.com/LZiRdQJ.png",
         duration: 60,
         difficulty: 4,
         tags: ['running', 'sprints', 'legs']
     },
     {
         title: 'Workout2',
-        img: String,
+        img: "https://i.imgur.com/KlTmSbc.png",
         duration: 30,
         difficulty: 2,
         tags: ['arms', 'lifting', 'bicep']
     },
     {
         title: 'Workout3',
-        img: String,
+        img: "https://i.imgur.com/254IdAG.png",
         duration: 45,
         difficulty: 3,
         tags: ['row', 'back', 'pullups']
     },
     {
         title: 'Workout4',
-        img: String,
+        img: "https://i.imgur.com/702hCGp.png",
         duration: 30,
         difficulty: 1,
         tags: ['chest', 'chestpress', 'dips']
     },
     {
         title: 'Workout5',
-        img: String,
+        img: "https://i.imgur.com/4tU0oya.png",
         duration: 60,
         difficulty: 5,
         tags: ['squats', 'deadlifts', 'hipthrust']
