fb0f5164293a4f66e74150262fd69179e7dc7068
rominadouk <douk.romina@gmail.com>
2023-02-27 09:33:00 -0800

    seeded data into mongoDB Atlas database, set a variable in my .env file

--- server.js
+++ server.js
@@ -89,8 +89,8 @@ app.listen(3000, () => {
     console.log('listening...');
 });
 
-mongoose.set('strictQuery', false);
 
-mongoose.connect('mongodb://localhost:27017/fitness', () => {
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGODB, () => {
     console.log('The connection with mongo is established.');
 });
